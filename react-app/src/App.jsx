1ed7f06ebbb3effdc145e7b66260a4b8482e5fd6
Muiz-2008 <afolabiabdulmuiz5@gmail.com>
2026-05-21 17:32:49 +0100

    Update App.jsx

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -7,7 +7,6 @@ import Category      from './pages/Category'
 import ArticleDetail from './pages/ArticleDetail'
 import Search        from './pages/Search'
 import About         from './pages/About'
-import Contact       from './pages/Contact'
 import NotFound      from './pages/NotFound'
 
 export default function App() {
@@ -20,7 +19,6 @@ export default function App() {
         <Route path="/article/:id"    element={<ArticleDetail />} />
         <Route path="/search"         element={<Search />} />
         <Route path="/about"          element={<About />} />
-        <Route path="/contact"        element={<Contact />} />
         <Route path="*"               element={<NotFound />} />
       </Routes>
       <Footer />
